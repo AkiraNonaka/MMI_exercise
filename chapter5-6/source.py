b68bad206ee59e6353124f2a7c43ac26a409f0fa
AkiraNonaka <38422016+AkiraNonaka@users.noreply.github.com>
2020-05-28 00:24:43 +0900

    Update source.py

--- chapter5-6/source.py
+++ chapter5-6/source.py
@@ -40,7 +40,7 @@ lm.fit(text_ngrams)
 context = ['好きな', 'な'] # 文脈の作成
 print(context, '->')
 
-prob_list = [[lm.source(w, context), for w in lm.context_counts(lm.vocab.lookup(context))]
+prob_list = [[lm.source(w, context), w] for w in lm.context_counts(lm.vocab.lookup(context))]
 prob_list.sort(reverse = True) # 出現確率順にソート
 
 for prob, word in prob_list[:10]:
