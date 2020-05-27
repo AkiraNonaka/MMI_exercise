1f88e955353cfc06e8cbd12728f59a43b9810775
AkiraNonaka <38422016+AkiraNonaka@users.noreply.github.com>
2020-05-28 00:00:13 +0900

    Update source3-2.py

--- chapter5-6/source3-2.py
+++ chapter5-6/source3-2.py
@@ -1,1 +1,9 @@
 from nltk.lm import Vocabulary
+from nltk.lm.models import MLE
+from nltk.util import ngrams
+
+N = 3
+vocab = Vocablary([word for sent in texts for word in sent], unk_cutoff=2, unl_label='<UNK>')
+text_ngrams = [ngrams(sent, N) for sent in texts]
+lm = MLE(order=N, vocablary=vocab)
+lm.fit(text_ngrams)
